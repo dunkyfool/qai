82b67c19d733ea162e650b1e203931a03de74158
dunkyfool <dunkyfool0113@gmail.com>
2016-06-02 17:33:47 +0800

    nothing

---
+++ model/seriesFrame_basicCNN.py
@@ -0,0 +1,10 @@
+from utils.layers import *
+
+#
+#  CNN3x3 - maxpool - CNN3x3 - maxpool - CNN3x3 - CNN3x3 FC FC softmax
+#  CNN3x3 - maxpool - CNN3x3 - maxpool - CNN1x1 - CNN3x3 FC FC softmax
+#
+
+
+if __name__=='__main__':
+  pass
