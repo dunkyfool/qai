8d5bf30a98c52ee32b9e56f6e49157a7a7a1286a
dunkyfool <dunkyfool0113@gmail.com>
2016-06-06 17:38:04 +0800

    stddev = 1/math.sqrt(in)

--- model/utils/initParams.py
+++ model/utils/initParams.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
+import math
 
 def weight(shape):
-  w = tf.truncated_normal(shape, stddev=1e-3)
+  w = tf.truncated_normal(shape, stddev=1/math.sqrt(float(shape[0])))
   return tf.Variable(w)
 
 def bias(shape):
