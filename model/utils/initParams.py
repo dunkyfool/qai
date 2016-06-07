f2195e1b908fec994e360c362066dcdb4c9c31bd
dunkyfool <dunkyfool0113@gmail.com>
2016-06-07 13:25:44 +0800

    w=>0.1

--- model/utils/initParams.py
+++ model/utils/initParams.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import math
 
 def weight(shape):
-  w = tf.truncated_normal(shape, stddev=1e-5)
+  w = tf.truncated_normal(shape, stddev=0.1)
   return tf.Variable(w)
 
 def bias(shape):
-  b = tf.constant(0.0, shape=shape)
+  b = tf.constant(0.1, shape=shape)
   return tf.Variable(b)
 
