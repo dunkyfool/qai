50bcf120abba92f84843dae08e7746451d2330e0
dunkyfool <dunkyfool0113@gmail.com>
2016-06-07 16:15:07 +0800

    first modelX

--- main.py
+++ main.py
@@ -4,5 +4,21 @@ from tensorflow.examples.tutorials.mnist import input_data
 import time
 
 if __name__=='__main__':
+  start = time.time()
+  _,_,_,_,testData,testLabel = load(path='data/CIFAR-10')
+  print testData.shape, testLabel.shape
+  print time.time()-start
 
+  # speedup test
+  testData = testData[0:100,:]
+  testLabel = testLabel[0:100,:]
+
+  testData = testData.reshape(-1,32,32,3)
+
+  _,H,W,C = testData.shape
+  _,cls = testLabel.shape
+
+  testData = testData-np.mean(testData,axis=0)
+  net = modelX()
+  net.loss(testData,testLabel)
   pass
