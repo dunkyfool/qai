f123f2af4fc669aa136728b9fea434d6eeb36d09
dunkyfool <dunkyfool0113@gmail.com>
2016-06-08 11:47:10 +0800

    script pull/push server

---
+++ pull.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp hduser@slave-1:/home/hduser/qai/main.py /Users/dunkyfool/QAI/
+scp hduser@slave-1:/home/hduser/qai/model/basicCNN.py /Users/dunkyfool/QAI/model/
---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp /Users/dunkyfool/QAI/main.py hduser@slave-1:/home/hduser/qai/
+scp /Users/dunkyfool/QAI/model/basicCNN.py hduser@slave-1:/home/hduser/qai/model/ 
