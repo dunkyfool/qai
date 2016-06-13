65dad85909935d027c230a1f84efc52ef47f32f4
dunkyfool <dunkyfool0113@gmail.com>
2016-06-13 13:38:59 +0800

    add layers

--- pull.sh
+++ pull.sh
@@ -3,3 +3,4 @@
 scp hduser@slave-1:/home/hduser/qai/main.py /Users/dunkyfool/QAI/
 scp hduser@slave-1:/home/hduser/qai/model/basicCNN.py /Users/dunkyfool/QAI/model/
 scp hduser@slave-1:/home/hduser/qai/model/utils/initParams.py /Users/dunkyfool/QAI/model/utils/
+scp hduser@slave-1:/home/hduser/qai/model/utils/layers.py /Users/dunkyfool/QAI/model/utils/
--- push.sh
+++ push.sh
@@ -3,3 +3,4 @@
 scp /Users/dunkyfool/QAI/main.py hduser@slave-1:/home/hduser/qai/
 scp /Users/dunkyfool/QAI/model/basicCNN.py hduser@slave-1:/home/hduser/qai/model/ 
 scp /Users/dunkyfool/QAI/model/utils/initParams.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
+scp /Users/dunkyfool/QAI/model/utils/layers.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
