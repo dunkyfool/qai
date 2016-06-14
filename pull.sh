15f1780c81c42f5ec47726a4384c8577da5ac940
dunkyfool <dunkyfool0113@gmail.com>
2016-06-14 13:29:27 +0800

    update

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 scp hduser@slave-1:/home/hduser/qai/main.py /Users/dunkyfool/QAI/
-scp hduser@slave-1:/home/hduser/qai/model/basicCNN.py /Users/dunkyfool/QAI/model/
-scp hduser@slave-1:/home/hduser/qai/model/utils/initParams.py /Users/dunkyfool/QAI/model/utils/
-scp hduser@slave-1:/home/hduser/qai/model/utils/layers.py /Users/dunkyfool/QAI/model/utils/
+scp -r hduser@slave-1:/home/hduser/qai/model/basicCNN*.py /Users/dunkyfool/QAI/model/
+#scp hduser@slave-1:/home/hduser/qai/model/utils/initParams.py /Users/dunkyfool/QAI/model/utils/
+#scp hduser@slave-1:/home/hduser/qai/model/utils/layers.py /Users/dunkyfool/QAI/model/utils/
+scp hduser@slave-1:/home/hduser/qai/model/utils/*.py /Users/dunkyfool/QAI/model/utils/
--- push.sh
+++ push.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 scp /Users/dunkyfool/QAI/main.py hduser@slave-1:/home/hduser/qai/
-scp /Users/dunkyfool/QAI/model/basicCNN.py hduser@slave-1:/home/hduser/qai/model/ 
-scp /Users/dunkyfool/QAI/model/utils/initParams.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
-scp /Users/dunkyfool/QAI/model/utils/layers.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
+scp -r /Users/dunkyfool/QAI/model/basicCNN*.py hduser@slave-1:/home/hduser/qai/model/ 
+#scp /Users/dunkyfool/QAI/model/utils/initParams.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
+#scp /Users/dunkyfool/QAI/model/utils/layers.py /hduser@slave-1:/home/hduser/qai/model/utils/ 
+scp -r /Users/dunkyfool/QAI/model/utils/*.py hduser@slave-1:/home/hduser/qai/model/utils/ 
